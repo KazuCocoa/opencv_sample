049c4e9a1634676b73792f7bf428713d2b6f9899
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-02-12 21:54:03 +0900

    add diff

--- lib/opencv.rb
+++ lib/opencv.rb
@@ -4,11 +4,14 @@ include OpenCV
 
 #画像を読み込む
 src_img = CvMat.load('images/PCuWF.png')
+src2_img = CvMat.load('images/rSqLE.png')
 
-gray_img = src_img.BGR2GRAY
-bin_img = gray_img.threshold(128, 255, :binary)
-canny_img = src_img.BGR2GRAY.canny(120,200)
+gray_img = src_img.BGR2GRAY # gray scale
+diff_img = src_img.abs_diff src2_img
+bin_diff_img = diff_img.threshold(0, 255, :binary)
+diff_sub = src_img.sub bin_diff_img
 
-gray_img.save_image "./images/results/gray_result.png"
-bin_img.save_image "./images/results/bin_result.png"
-canny_img.save_image "./images/results/canny_result.png"
+gray_img.save_image "./images/results/gray_img.png"
+diff_img.save_image "./images/results/diff_result.png"
+bin_diff_img.save_image "./images/results/bin_diff_img.png"
+diff_sub.save_image "./images/results/diff_sub.png"
